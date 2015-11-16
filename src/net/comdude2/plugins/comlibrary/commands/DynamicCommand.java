f7515928283ad351e1ce1f24f00f3d242b9ed589
BuildTools <unconfigured@null.spigotmc.org>
2015-11-16 01:40:08 +0000

    First commit

---
+++ src/net/comdude2/plugins/comlibrary/commands/DynamicCommand.java
@@ -0,0 +1,7 @@
+package net.comdude2.plugins.comlibrary.commands;
+
+/**
+ * Created by hi on 16/11/2015.
+ */
+public class DynamicCommand {
+}
---
+++ src/net/comdude2/plugins/comlibrary/commands/StaticCommand.java
@@ -0,0 +1,7 @@
+package net.comdude2.plugins.comlibrary.commands;
+
+/**
+ * Created on 16/11/2015.
+ */
+public class StaticCommand {
+}
