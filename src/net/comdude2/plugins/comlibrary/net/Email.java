95b61a9d42ae04cd78eb6e4449c89930bf2aec9b
Matt <comdude2@msn.com>
2015-11-21 15:43:50 +0000

    - Added JavaMail api, not tested it yet.

---
+++ src/net/comdude2/plugins/comlibrary/net/Email.java
@@ -0,0 +1,31 @@
+package net.comdude2.plugins.comlibrary.net;
+
+import java.util.LinkedList;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Email {
+	
+	public static boolean send(String to, LinkedList <String> cc, String from, String smtpHost, String subject, String text) throws AddressException, MessagingException{
+		Properties props = System.getProperties();
+		Session session = Session.getDefaultInstance(props);
+		MimeMessage message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(from));
+		message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+		for (String s : cc){
+			message.addRecipient(Message.RecipientType.CC, new InternetAddress(s));
+		}
+		message.setSubject(subject);
+		message.setText(text);
+		Transport.send(message);
+		return true;
+	}
+	
+}
